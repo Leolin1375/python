2c1c51d173aff1f42870cb5cca79c9f1efd3853b
leo <leolin@goodwill.com.tw>
2024-06-13 12:59:44 +0800

    列表

---
+++ 列表/test1.py
@@ -0,0 +1,22 @@
+# #!/usr/bin/python3
+
+# list = ['red', 'green', 'blue', 'yellow', 'white', 'black']
+# print( list[0] )
+# print( list[1] )
+# print( list[2] )
+
+# #!/usr/bin/python3
+
+# list = ['red', 'green', 'blue', 'yellow', 'white', 'black']
+# print( list[-1] )
+# print( list[-2] )
+# print( list[-3] )
+
+#!/usr/bin/python3
+
+nums = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+print(nums[0])
+print(nums[1])
+print(nums[2])
+
+print(nums[0:4])
